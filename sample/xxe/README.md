54af6604f4591406b13bf76f5d98e0e21b09ef61
No1zy <bgkn12601208@gn.iwasaki.ac.jp>
2019-09-12 21:14:15 +0900

    Add xxe template

---
+++ sample/xxe/README.md
@@ -0,0 +1,12 @@
+# XXE のテンプレート
+対応言語
+* java 
+* php
+* python
+
+## example
+specify xml file
+
+```xml
+go run main.go --service java --template sample/xxe/ test.xml
+```
---
+++ sample/xxe/template.java
@@ -0,0 +1,21 @@
+import java.io.FileReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String filename = "{{ .FILENAME }}";
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new InputSource(filename));
+        String data = doc.getElementsByTagName("data").item(0).getTextContent();
+
+        System.out.println("data: " + data);
+    }
+}
